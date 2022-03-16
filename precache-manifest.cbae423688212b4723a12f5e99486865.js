813e7e73cf402af1b777c6b8b17a768a8439a660
Alex Zaks <zaksa86@gmail.com>
2022-03-16 04:00:58 -0400

    Updates

---
+++ precache-manifest.cbae423688212b4723a12f5e99486865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4366bafe389aecebb6c4fbda2daf3109",
+    "url": "/SocialPosts/index.html"
+  },
+  {
+    "revision": "e2c872250a00a7192209",
+    "url": "/SocialPosts/static/css/main.8efbc0c0.chunk.css"
+  },
+  {
+    "revision": "7115df74be34a2b62584",
+    "url": "/SocialPosts/static/js/2.4ba44835.chunk.js"
+  },
+  {
+    "revision": "1ce896dddecdb4d51806",
+    "url": "/SocialPosts/static/js/3.c541d426.chunk.js"
+  },
+  {
+    "revision": "e2c872250a00a7192209",
+    "url": "/SocialPosts/static/js/main.3e4513c6.chunk.js"
+  },
+  {
+    "revision": "2c950a7599f3cf06431e",
+    "url": "/SocialPosts/static/js/runtime~main.86ed1566.js"
+  }
+]);
