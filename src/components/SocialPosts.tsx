f05d4828e4d996dc9af193d25e606753db692702
Alex Zaks <zaksa86@gmail.com>
2022-03-16 03:59:38 -0400

    ready to deploy

--- src/components/SocialPosts.tsx
+++ src/components/SocialPosts.tsx
@@ -8,9 +8,9 @@ import PostForm from "./PostForm"
 export default function SocialPosts() {
 
     const [posts, setPost] = useState<Post[]>([
-        {title: "My NFT is worth Millions!", thought: "You guys gotta get on this!"},
-        {title: "Delorean for Sale", thought: "Wait until this baby hits 88mph"},
-        {title: "Time Travel is real!", thought: "All you need is a Flux Capacitor!"}
+        {title: "Howdy Partners", thought: "Learning React is a blast!"},
+        {title: "Burritos", thought: "They taste good with guacamole"},
+        {title: "Used Delorean for Sale", thought: "Wait until this baby hits 88mph"}
     ]);
 
     const [hidden, setHidden] = useState<boolean>(true);
@@ -20,23 +20,35 @@ export default function SocialPosts() {
         setPost((prev) => [...posts, post]);
         setHidden(true)
     }
-
+    
     function handleDeletePost(index: number) {
         setPost((prev) => [...prev.slice(0, index), ...prev.slice(index +1)])
-    }
+    } 
 
 return (
 <div className="PostsParent">
     <h1>My thoughts</h1>
-    <button className="NewThought" onClick={ ()=> setHidden(false)}>New Thought</button>
     <div className="PostsMap">
         {posts.map((post, i) => (
-        <PostInList key={i} post={post}  onDelete={ ()=> handleDeletePost(i)} />
+        <PostInList 
+        key={i} 
+        post={post}  
+        onDelete={ ()=> handleDeletePost(i)} />
         ))}
         <div>
-        {!hidden ? <div className="blurBG"><p></p></div> : null}
-        {!hidden ? <PostForm onSubmit={handlePostFormSubmit} onClose={() => setHidden(true)} /> : null}</div>
+
+        {!hidden ? 
+        <div className="blurBG"><p></p></div> 
+        : null}
+
+        {!hidden ?
+        <PostForm 
+        onSubmit={handlePostFormSubmit} 
+        onClose={() => setHidden(true)} /> 
+        : null}</div>
+        
         </div>
+        <button className="NewThought" onClick={ ()=> setHidden(false)}>New Thought</button>
 
 </div>
 
